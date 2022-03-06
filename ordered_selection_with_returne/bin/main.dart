65666d40786328cb9060d576365a191e57f5bba6
Shteyd <shteyd.dev@gmail.com>
2022-03-06 16:31:30 +0300

    add first app

---
+++ ordered_selection_with_returne/bin/main.dart
@@ -0,0 +1,66 @@
+import 'dart:convert';
+import 'dart:io';
+import 'dart:math';
+import 'package:path/path.dart' as path;
+
+const List<int> store = [1, 2, 3, 4];
+
+void main(List<String> arguments) {
+  const JsonEncoder encoder = JsonEncoder.withIndent('  ');
+  final Map<String, int> map = _dataForJson;
+
+  writeFile(encoder.convert(map));
+}
+
+Future<String> get dirPath async {
+  final Directory dir = Directory('${path.current}\\data');
+
+  if (await dir.exists()) {
+    return dir.path;
+  }
+
+  final Directory _ = await dir.create(recursive: true);
+  return _.path;
+}
+
+writeFile(String json) async {
+  final localPath = await dirPath;
+  final file = File('$localPath\\data.json');
+
+  if (await file.exists()) {
+    file.delete(recursive: true);
+  } else {
+    file.create(recursive: true);
+  }
+  file.writeAsString(json);
+}
+
+Map<String, int> get _mapKeys {
+  final Map<String, int> _map = {};
+  for (int i = 111; i < 445; i++) {
+    final String _key = i.toString();
+    if (_key.contains(RegExp(r'[0, 5-9]'))) {
+      continue;
+    }
+    _map[_key] = 0;
+  }
+  return _map;
+}
+
+Map<String, int> get _dataForJson {
+  final Map<String, int> _map = _mapKeys;
+  final Random random = Random();
+
+  for (int counter = 0; counter <= 6400; counter++) {
+    // max length is 3
+    final List<int> cells = [];
+
+    for (int cell = 0; cell < 3; cell++) {
+      cells.add(store[random.nextInt(4)]);
+    }
+
+    _map[cells.join()] = _map[cells.join()]! + 1;
+  }
+
+  return _map;
+}
